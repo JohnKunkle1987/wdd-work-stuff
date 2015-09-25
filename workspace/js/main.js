34543adde0d998164848a08efcc7ee35315cd89d
johnkunkle <myvyrus@fullsail.edu>
2015-09-25 18:28:41 +0000

    your message goes here

--- workspace/js/main.js
+++ workspace/js/main.js
@@ -13,8 +13,27 @@ $(document).ready(function(){
     $("footer section #up").click(function() {
     $('html, body').animate({
         scrollTop: $("#launch").offset().top
-    }, 1000);
+   }, 1000);
 });
 
     
+	
+	$(".solBtn").click(function(){
+			console.log("clicked");
+			if ($(this).text() === "Solution")
+			{
+				console.log("and true");
+				$(this).siblings("div").slideDown(1000);
+				//$(this).parent().css("padding", "0px");
+				$(this).text("Hide");
+			}
+			else if ($(this).text() === "Hide")
+			{
+				console.log("and false");
+				$(this).siblings("div").slideUp(1000);
+			//	$(this).parent().css("padding", "15px");
+				$(this).text("Solution");
+			}
+		});
+	
 });
